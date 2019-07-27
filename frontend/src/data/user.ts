90c1cc31be593a8e50ee95255d18043754a362a4
v-raina <alina.aldhytha@gmail.com>
2019-07-27 23:05:30 +1000

    added user data

---
+++ frontend/src/data/user.ts
@@ -0,0 +1,15 @@
+export const student = {
+	name: 'Sabrina Soon',
+	caption: 'I am a Year 6 student in Malaysia who likes reading and coding.',
+	dob: '2 February 2007',
+	age: '12',
+	hobby: 'reading, writing and coding!',
+	goal: 'I want to enrol in High School to further my studies.',
+	dream: 'I want to travel around the world!',
+	fav_subject: 'Mathematics',
+};
+
+export const sponsor = {
+	name: 'Joseph Yeoh',
+	is_anon: 'no',
+};
