ade53eb9a2b02b55167af37a4378e831d3cc1852
Zi Jing Yeoh <z5108653@student.unsw.edu.au>
2019-07-27 15:41:40 +1000

    bug fixes sorry

--- frontend/src/constants/fonts.ts
+++
@@ -1,4 +0,0 @@
-export const FONT_FAMILY_POPPINS = '"Poppins"';
-export const FONT_FAMILY_SANS_SERIF = 'sans-serif';
-
-export const FONT_FAMILY_DEFAULT = `${FONT_FAMILY_POPPINS}, ${FONT_FAMILY_SANS_SERIF}`;
