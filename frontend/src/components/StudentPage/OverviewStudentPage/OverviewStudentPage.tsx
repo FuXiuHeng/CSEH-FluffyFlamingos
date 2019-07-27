cbf18d6b4e145d921b963f3f54eb55f9b6796a31
FuXiuHeng <hengfuxiu@gmail.com>
2019-07-28 00:46:21 +1000

    Complete overview

--- frontend/src/components/StudentPage/OverviewStudentPage/OverviewStudentPage.tsx
+++ frontend/src/components/StudentPage/OverviewStudentPage/OverviewStudentPage.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { withStyles, WithStyles } from '@material-ui/core/styles';
+import { Typography } from '@material-ui/core';
 
+import videoImg from './video.png';
+import studentImg from '../student.jpg';
 import { styles } from './styles';
 
 export interface OverviewStudentPageProps extends WithStyles<typeof styles> {}
@@ -12,7 +15,44 @@ class PureOverviewStudentPage extends React.Component<
 > {
 	render() {
 		const { classes } = this.props;
-		return <div className={classes.root}>Overview</div>;
+		return (
+			<div className={classes.root}>
+				<div className={classes.topPanel}>
+					<div>
+						<Typography className={classes.header} variant="body1">
+							<b>More about myself</b>
+						</Typography>
+						<Typography variant='body1'>
+							Age: 20<br/>
+							My favourite subject: Maths<br/>
+							Aspiration: Software Engineer<br/>
+							Hobby: Reading<br/>
+							Live in: Malaysia<br/>
+						</Typography>
+					</div>
+					<div className={classes.imageContainer}>
+						<img className={classes.studentImage} src={studentImg} alt="" />
+						<img className={classes.overlayImage} src={videoImg} alt="" />
+					</div>
+				</div>
+				<br/>
+				<Typography>
+					I want to study further to level up my studies in order to strive for a better future for my family
+					Due to unstable financial conditions, I had to drop my education at the age of 17. With 4 children in
+					the household, my parents are not able to afford higher education for all of us. I also helped my parents
+					work in the restaurant to earn more income and help the family. 	
+				</Typography>
+				<Typography className={classes.header} variant="body1">
+					<b>How the Scholarship helps Sabrina</b>
+				</Typography>
+				<Typography variant='body1'>
+					The raised funds will help secure a scholarship for Sabrina to 
+					empower her to attain a better future. This scholarship will be 
+					used to enroll her into the University of Malaysia, who has partnered
+					with us in the scholarship program.
+				</Typography>
+			</div>
+		);
 	}
 }
 
--- frontend/src/components/StudentPage/OverviewStudentPage/styles.ts
+++ frontend/src/components/StudentPage/OverviewStudentPage/styles.ts
@@ -1,8 +1,34 @@
 import { createStyles } from '@material-ui/core/styles';
+import { Opacity } from '@material-ui/icons';
 
 export const styles = theme =>
 	createStyles({
 		root: {
+			boxShadow: '1px 1px 5px -2px #888',
+			margin: '10px',
 			padding: '10px',
 		},
+		header: {
+			marginTop: 12,
+		},
+		topPanel: {
+			display: 'grid',
+			gridTemplateColumns: '2fr 1fr'
+		},
+		imageContainer: {
+			position:'relative',
+			width: 250,
+		},
+		studentImage: {
+			width: '100%'
+		},
+		overlayImage: {
+			position: "absolute",
+			top: '50%',
+			left: '50%',
+			width: 'auto',
+			height: '60%',
+			transform: 'translate(-50%, -50%)',
+			opacity: 0.7
+		}
 	});
