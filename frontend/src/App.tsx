6920c6496ac41a36a4b7ce135285c0fa7d6ed42f
Zi Jing Yeoh <z5108653@student.unsw.edu.au>
2019-07-27 17:12:45 +1000

    import problems solved

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Router, Route } from 'react-router';
+import history from './history';
+import SponsorPage from './components/SponsorPage';
 
 const App: React.FC = () => {
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
-		</div>
+		<Router history={history}>
+			<Route exact path="/" component={SponsorPage} />
+			<Route path="/homePage" component={undefined} />
+			<Route path="/sponsorPage" component={SponsorPage} />
+		</Router>
 	);
 };
 
