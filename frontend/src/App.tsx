61d6448fa61dab7e266d157ad99695468d58bad5
Zi Jing Yeoh <z5108653@student.unsw.edu.au>
2019-07-27 18:55:31 +1000

    Updated StudentPage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,39 +1,38 @@
 import React from 'react';
 import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
-import "./App.css";
-import { Router, Route } from "react-router";
-import history from "./history";
-import SponsorPage from "./components/SponsorPage";
-import { 
-  NavBar,
-  StudentHomePage
-} from "./components";
+import './App.css';
+import { Router, Route } from 'react-router';
+import history from './history';
+import SponsorPage from './components/SponsorPage';
+import { NavBar, StudentHomePage, StudentPage } from './components';
 
 const theme = createMuiTheme({
 	palette: {
 		primary: {
-			main: '#0f0326',
+			main: '#23395b',
+			light: '#90D7FF',
 		},
 		secondary: {
-			main: '#ea526f',
+			main: '#ef6461',
 		},
 	},
 });
 
 const App: React.FC = () => {
-  return (
-    <div>
-      <MuiThemeProvider theme={theme}>
-        <NavBar />
-        <Router history={history}>
-          <Route exact path="/" component={SponsorPage} />
-          <Route path="/homePage" component={undefined} />
-          <Route path="/sponsorPage" component={SponsorPage} />
-          <Route path="/studentHomePage" component={StudentHomePage} />
-        </Router>
-      </MuiThemeProvider>
-    </div>
-  );
+	return (
+		<div>
+			<MuiThemeProvider theme={theme}>
+				<NavBar />
+				<Router history={history}>
+					<Route exact path="/" component={SponsorPage} />
+					<Route path="/homePage" component={undefined} />
+					<Route path="/sponsorPage" component={SponsorPage} />
+					<Route path="/studentHomePage" component={StudentHomePage} />
+					<Route path="/studentPage" component={StudentPage} />
+				</Router>
+			</MuiThemeProvider>
+		</div>
+	);
 };
 
 export default App;
--- frontend/src/components/StudentPage/StudentPage.tsx
+++ frontend/src/components/StudentPage/StudentPage.tsx
@@ -1,19 +1,47 @@
 import React from 'react';
-// import { colors, dimensions, sizes } from 'constants';
-import './StudentPage.css';
+import { withStyles, WithStyles } from '@material-ui/core/styles';
+import { Typography, Tabs, Tab } from '@material-ui/core';
 
-export interface Props {}
-export interface State {}
+import { styles } from './styles';
+import student from './student.jpg';
 
-class StudentPage extends React.Component<Props, State> {
-	state: State = {};
+export interface StudentPageProps extends WithStyles<typeof styles> {}
+export interface StudentPageState {}
 
+class PureStudentPage extends React.Component<StudentPageProps, StudentPageState> {
 	render() {
-		const { state, props } = this;
+		const { classes } = this.props;
 		return (
-			null
+			<div className={classes.root}>
+				<div className={classes.studentCard}>
+					<div className={classes.photoContainer}>
+						<img className={classes.studentPhoto} src={student} alt="student" />
+					</div>
+					<div className={classes.studentNameContainer}>
+						<div className={classes.studentName}>
+							<Typography variant="h2">Sabrina Soon</Typography>
+							<Typography variant="h4">
+								I am a Year 5 student in Malaysia who likes reading and coding!
+							</Typography>
+						</div>
+					</div>
+				</div>
+				<div>
+					{/* <Tabs
+						value={value}
+						indicatorColor="primary"
+						textColor="primary"
+						onChange={handleChange}
+						aria-label="disabled tabs example"
+					>
+						<Tab label="Active" />
+						<Tab label="Disabled" disabled />
+						<Tab label="Active" />
+					</Tabs> */}
+				</div>
+			</div>
 		);
 	}
 }
 
-export default StudentPage;
+export const StudentPage = withStyles(styles)(PureStudentPage);
